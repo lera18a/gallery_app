015b93b9258a1a7e5d9d86f5b85803f3cd57a795
Badrakova Valeria <lera_18a@mail.ru>
2026-01-30 17:48:54 +0300

    initial commit

---
+++ GalleryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GalleryApp
+//
+//  Created by Valeria Badrakova on 30.01.26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
